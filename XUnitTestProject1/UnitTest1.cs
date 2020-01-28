96e4b210b86c513ccf66e3c26559fc80a33899c4
venkatesh kalva <venkateshkalva@yahoo.in>
2020-01-27 23:29:03 -0600

    Create a Mock Data base for perisitance testing. create MockDatabaseHelper class with Microsoft.EntityFrameworkCore.InMemory

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+public static class MockDatabaseHelper
+{
+    public static DbContextOptions<WeatherDbContext> CreateNewContextOptions(string databaseName)
+    {
+        //Create a fresh service provider, and therefore a fresh    
+        // InMemory database instance    
+        var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+        // Create a new options instance telling the context to use an    
+        // InMemory database and the new service provider    
+        var builder = new DbContextOptionsBuilder<WeatherDbContext>();
+        builder.UseInMemoryDatabase(databaseName)
+            .UseInternalServiceProvider(serviceProvider);
+        return builder.Options;
+    }
+}
