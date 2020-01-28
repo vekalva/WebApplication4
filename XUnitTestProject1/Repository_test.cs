a885fee1ecc661424a35541c51c2445ab9027b8c
venkatesh kalva <venkateshkalva@yahoo.in>
2020-01-27 23:31:03 -0600

    create unit test for the generic repository class.Then add test cases.

---
+++ XUnitTestProject1/Repository_test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Weather.Persistence.Config;
+using Weather.Persistence.Models;
+using Weather.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using Serilog;
+using Shouldly;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace Weather.Test.Persistence
+{
+    public class RepositoryTest
+    {
+        private DbContextOptions<WeatherDbContext> _contextOptions;
+        private City _testData;
+        private WeatherDbContext _appContext;
+        private IOptions<DbContextSettings> _settings;
+        private IDbContextFactory _dbContextFactory;
+        private Repository<City> _subject;
+        private City _result;
+
+        public RepositoryTest()
+        {
+            _testData = new City
+            {
+                Id = "26216",
+                Name = "Melbourne",
+                CountryId = "AU",
+                AccessedDate = new DateTime(2018, 12, 29, 10, 1, 2)
+            };
+
+        }
+    }
